263a07dc12b80074d0dfbeab2e83feb44e822ff3
ljy <393941932@qq.com>
2020-02-27 18:18:41 +0900

    Initial commit

---
+++ blink-manager/src/main/java/com/blink/BlinkManagerApplication.java
@@ -0,0 +1,13 @@
+package com.blink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkManagerApplication.class, args);
+	}
+
+}
---
+++ blink-manager/src/test/java/com/blink/BlinkManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
